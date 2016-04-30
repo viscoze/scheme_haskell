2e66b746247bc4ddbacaaa63252d78f8018f54e3
Viscoze <vlad.oganov@gmail.com>
2016-05-01 01:24:09 +0300

    add basic evaluation

--- Main.hs
+++ Main.hs
@@ -5,10 +5,7 @@ import System.Environment
 import Text.ParserCombinators.Parsec hiding (spaces)
 
 main :: IO ()
-main = do
-	args <- getArgs
-	putStrLn $ readExpr $ args !! 0
-
+main = getArgs >>= putStrLn . show . eval . readExpr . (!! 0)
 
 data LispVal = Atom String
 						 | List [LispVal]
@@ -16,6 +13,49 @@ data LispVal = Atom String
 						 | Number Integer
 						 | String String
 						 | Bool Bool
+
+instance Show LispVal where
+	show = showVal
+
+eval :: LispVal -> LispVal
+eval val@(String _) = val
+eval val@(Number _) = val
+eval val@(Bool _)   = val
+eval (List [Atom "quote", val]) = val
+eval (List (Atom func : args)) = apply func $ map eval args
+
+apply :: String -> [LispVal] -> LispVal
+apply func args = maybe (Bool False) ($ args) $ lookup func primitives
+
+showVal :: LispVal -> String
+showVal (String contents) = "\"" ++ contents ++ "\""
+showVal (Atom name) = name
+showVal (Number contents) = show contents
+showVal (Bool True) = "#t"
+showVal (Bool False) = "#f"
+showVal (List contents) = "(" ++ unwordsList contents ++ ")"
+showVal (DottedList head tail) = "(" ++ unwordsList head ++
+													       " . " ++ showVal tail ++ ")"
+
+primitives :: [(String, [LispVal] -> LispVal)]
+primitives = [("+", numericBinop (+)),
+							("-", numericBinop (-)),
+							("/", numericBinop div),
+							("mod", numericBinop mod),
+							("quotient", numericBinop quot),
+							("remainder", numericBinop rem)]
+
+numericBinop :: (Integer -> Integer -> Integer) -> [LispVal] -> LispVal
+numericBinop op params = Number $ foldl1 op $ map unpackNum params
+
+unpackNum :: LispVal -> Integer
+unpackNum (Number n) = n
+unpackNum (String n) = let parsed = reads n in
+													if null parsed
+														then 0
+														else fst $ parsed !! 0
+unpackNum (List [n]) = unpackNum n 
+unpackNum _ = 0
 
 parseString :: Parser LispVal
 parseString = do
@@ -69,7 +109,10 @@ spaces = skipMany1 space
 symbol :: Parser Char
 symbol = oneOf "!$ %&|*+ -/: <=? > @^_ ~# "
 
+unwordsList :: [LispVal] -> String
+unwordsList = unwords . map showVal
+
 readExpr :: String -> String
 readExpr input = case parse parseExpr "lisp" input of
- Left err  -> "No match: " ++ show err
- Right val -> "Found value"
+ Left err  -> String $ "No match: " ++ show err
+ Right val -> val
