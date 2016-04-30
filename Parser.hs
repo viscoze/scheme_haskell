4b0364e551a0a34c607edd621002d739649583ed
Viscoze <vlad.oganov@gmail.com>
2016-04-30 23:41:03 +0300

    add parser for dottedlist, list

--- Parser.hs
+++ Parser.hs
@@ -23,6 +23,31 @@ parseAtom = do
 parseNumber :: Parser LispVal
 parseNumber = liftM (Number . read) $ many1 digit
 
+parseExpr :: Parser LispVal
+parseExpr = parseAtom
+        <|> parseString
+        <|> parseNumber
+        <|> parseQuoted
+        <|> do char '('
+                x <- (try parseList) <|> parseDottedList
+                char ')'
+                return x
+
+parseList :: Parser LispVal
+parseList = liftM List $ sepBy parseExpr spaces
+
+parseDottedList :: Parser LispVal
+parseDottedList = do
+  head <- endBy parseExpr spaces
+  tail <- char '.' >> spaces >> parseExpr
+  return $ DottedList head tail
+
+parseQuoted :: Parser LispVal
+parseQuoted = do
+  char '\''
+  x <- parseExpr
+  return $ List [Atom "quote", x]
+
 spaces :: Parser ()
 spaces = skipMany1 space
 
@@ -30,6 +55,6 @@ symbol :: Parser Char
 symbol = oneOf "!$ %&|*+ -/: <=? > @^_ ~# "
 
 readExpr :: String -> String
-readExpr input = case parse (spaces >> symbol) "lisp" input of
+readExpr input = case parse parseExpr "lisp" input of
   Left err  -> "No match: " ++ show err
   Right val -> "Found value"
