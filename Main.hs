75b0638f12793b8adceff8f2b84754e6d0c1a079
Viscoze <vlad.oganov@gmail.com>
2016-05-01 09:47:44 +0300

    add eq eqv equal

--- Main.hs
+++ Main.hs
@@ -21,7 +21,6 @@ data LispVal = Atom String
 instance Show LispVal where
 	show = showVal
 
-
 data LispError = NumArgs Integer [LispVal]
 							 | TypeMismatch String LispVal
 							 | Parser ParseError
@@ -54,13 +53,69 @@ trapError action = catchError action (return . show)
 extractValue :: throwError a -> a
 extractValue (Right val) = val
 
+data Unpacker = forall a. Eq a => AnyUnpacker (LispVal -> ThrowsError a)
+
+unpackEquals :: LispVal -> LispVal -> Unpacker -> ThrowsError Bool
+unpackEquals arg1 arg2 (AnyUnpacker unpacker) =
+						 do unpacked1 <- unpacker arg1
+						 		unpacked2 <- unpacker arg2
+								return $ unpacked1 == unpacked2
+			'catchError' (const $ return False)
+
 eval :: LispVal -> LispVal
 eval val@(String _) = return val
 eval val@(Number _) = return val
 eval val@(Bool _)   = return val
 eval (List [Atom "quote", val]) = return val
+eval (List [Atom "if", pred, conseq, alt]) =
+	do result <- eval pred
+		 case result of
+			 Bool False -> eval alt
+			 otherwise  -> eval conseq
 eval (List (Atom func : args)) = mapM eval args >>= apply func
 eval badForm = throwError $ BadSpecialForm "Unrecognized special form" badForm
+
+car :: [LispVal] -> ThrowsError LispVal
+car [List (x : xs)] = return x
+car [DottedList (x : xs) _] = return x
+car [badArg] = throwError $ TypeMismatch "pair" badArg
+car badArgList = throwError $ NumArgs 1 badArgList
+
+cdr :: [LispVal] -> ThrowsError LispVal
+cdr [List (x : xs)] = return $ List xs
+cdr [DottedList (_ : xs) x] = return $  DottedList xs x
+cdr [badArg] = throwError $ TypeMismatch "pair" badArg
+cdr badArgList = throwError $ NumArgs 1 badArgList
+
+cons :: [LispVal] -> ThrowsError LispVal
+cons [x1, List []] = return $ List [x1]
+cons [x, List xs]  = return $ List $ [x] ++ xs
+cons [x, DottedList xs xlast] = return $ DottedList ([x] ++ xs) xlast
+cons [x1, x1] = return $ DottedList [x1] x2
+cons badArgList = throwError $ NumArgs 2 badArgList
+
+eqv :: [LispVal] -> ThrowsError LispVal
+eqv [(Bool arg1), (Bool arg2)] = return $ Bool $ arg1 == arg2
+eqv [(Number arg1), (Number arg2)] = return $ Bool $ arg1 == arg2
+eqv [(String arg1), (String arg2)] = return $ Bool $ arg1 == arg2
+eqv [(Atom arg1), (Atom arg2)] = return $ Bool $ arg1 == arg2
+eqv [(DottedList xs x), (DottedList ys y)] = eqv [List $ xs ++ [x], List $ ys ++ [y]]
+eqv [(List arg1), (List arg2)] = return $ Bool $ (length arg1 == length arg2) &&
+																 (and $ map eqvPair $ zip arg1 arg2)
+																 where eqvPair (x1,x2) = case eqv [x1,x2] of
+																	 												 Lefr err -> False
+																													 Right (Bool val) -> val
+eqv [_, _] = return $ Bool False
+eqv badArgList = throwError $ NumArgs 2 badArgList
+
+equal :: [ListVal] -> ThrowsError LispVal
+equal [arg1, arg2] = do
+	primitiveEquals <- liftM or $ mapM (unpackEquals arg1 arg2)
+										[ AnyUnpacker unpackNum , AnyUnpacker
+											unpackStr , AnyUnpacker unpackBool ]
+	eqvEquals <- eqv [arg1, arg2]
+	return $ Bool $ (primitiveEquals || let (Bool x) = eqvEquals in x)
+equal badArgList = throwError $ NumArgs 2 badArgList
 
 apply :: String -> [LispVal] -> throwError LispVal
 apply func args = maybe (throwError $ NotFunction "Unrecognized primitive function args ")
@@ -96,7 +151,13 @@ primitives = [("+", numericBinop (+)),
 							("string<?", strBoolBinop (<)),
 							("string>?", strBoolBinop (>)),
 							("string<=?", strBoolBinop (<=)),
-							("string>=?", strBoolBinop (>=))]
+							("string>=?", strBoolBinop (>=)),
+							("car", car),
+							("cdr", cdr),
+							("cons", cons),
+							("eq?", eqv),
+							("eqv?", eqv),
+							("equal ?", equal)]
 
 numBoolBinop = boolBinop unpackNum
 strBoolBinop = boolBinop unpackStr
